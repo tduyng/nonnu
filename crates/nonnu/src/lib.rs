c277a50d306122c3e61c71ba1f22d03beea49992
tduyng <tienduy.nguyen.dev@gmail.com>
2024-04-22 21:16:39 +0200

    feat(nonnu): add parser

--- crates/nonnu/src/lib.rs
+++ crates/nonnu/src/lib.rs
@@ -1,7 +1,29 @@
+use env::Env;
+use val::Val;
+
 pub mod binding_def;
 pub mod expr;
 pub mod stmt;
 pub mod val;
+pub mod env;
 
-mod env;
 mod utils;
+
+
+#[derive(Debug)]
+pub struct Parse(stmt::Stmt);
+
+impl Parse {
+    pub fn eval(&self, env: &mut Env) -> Result<Val, String>{
+        self.0.eval(env)
+    }
+}
+
+pub fn parse(s: &str) -> Result<Parse, String>{
+    let (s, stmt) = stmt::Stmt::new(s)?;
+    if s.is_empty(){
+        Ok(Parse(stmt))
+    } else {
+        Err("input was not consumed fully by parser".to_string())
+    }
+}
