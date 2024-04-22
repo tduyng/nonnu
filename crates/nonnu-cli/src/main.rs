b6cec5f1bc7180574ddb82c3ea3329537ff7c912
tduyng <tienduy.nguyen.dev@gmail.com>
2024-04-22 21:26:14 +0200

    feat(cli): add basic REPL

--- crates/nonnu-cli/src/main.rs
+++ crates/nonnu-cli/src/main.rs
@@ -1,3 +1,35 @@
-fn main(){
-    println!("Hello Nonnu cli!")
+use nonnu::env::Env;
+use std::io::{self, Write};
+
+fn main() -> io::Result<()> {
+    let stdin = io::stdin();
+    let mut stdout = io::stdout();
+    let mut stderr = io::stderr();
+    let mut input = String::new();
+    let mut env = Env::default();
+
+    loop {
+        write!(stdout, "→ ")?;
+        stdout.flush()?;
+        stdin.read_line(&mut input)?;
+
+        match run(input.trim(), &mut env) {
+            Ok(()) => {}
+            Err(msg) => {
+                writeln!(stderr, "{}", msg)?;
+                stderr.flush()?;
+            }
+        }
+
+        input.clear();
+    }
+}
+
+fn run(input: &str, env: &mut Env) -> Result<(), String> {
+    let parse = nonnu::parse(input).map_err(|msg| format!("parse error: {}", msg))?;
+    let evaluated = parse.eval(env).map_err(|msg| format!("evaluation error: {}", msg))?;
+
+    dbg!(evaluated);
+
+    Ok(())
 }
--- crates/nonnu/src/lib.rs
+++ crates/nonnu/src/lib.rs
@@ -2,26 +2,25 @@ use env::Env;
 use val::Val;
 
 pub mod binding_def;
+pub mod env;
 pub mod expr;
 pub mod stmt;
 pub mod val;
-pub mod env;
 
 mod utils;
-
 
 #[derive(Debug)]
 pub struct Parse(stmt::Stmt);
 
 impl Parse {
-    pub fn eval(&self, env: &mut Env) -> Result<Val, String>{
+    pub fn eval(&self, env: &mut Env) -> Result<Val, String> {
         self.0.eval(env)
     }
 }
 
-pub fn parse(s: &str) -> Result<Parse, String>{
+pub fn parse(s: &str) -> Result<Parse, String> {
     let (s, stmt) = stmt::Stmt::new(s)?;
-    if s.is_empty(){
+    if s.is_empty() {
         Ok(Parse(stmt))
     } else {
         Err("input was not consumed fully by parser".to_string())
