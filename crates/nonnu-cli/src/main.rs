9d089de7c0d3faf88e112532adb93ed16977832b
tduyng <tienduy.nguyen.dev@gmail.com>
2024-04-23 19:09:51 +0200

    feat: wip everything, try implement other ways

--- crates/nonnu-cli/src/main.rs
+++ crates/nonnu-cli/src/main.rs
@@ -1,36 +1,3 @@
-use std::io::{self, Write};
-
-use nonnu::{Env, Val};
-
-fn main() -> io::Result<()> {
-    let stdin = io::stdin();
-    let mut stdout = io::stdout();
-    let mut stderr = io::stderr();
-    let mut input = String::new();
-    let mut env = Env::default();
-
-    loop {
-        write!(stdout, "→ ")?;
-        stdout.flush()?;
-        stdin.read_line(&mut input)?;
-
-        match run(input.trim(), &mut env) {
-            Ok(Some(val)) => writeln!(stdout, "{}", val)?,
-            Ok(None) => {}
-            Err(msg) => writeln!(stderr, "{}", msg)?,
-        }
-
-        input.clear();
-    }
-}
-
-fn run(input: &str, env: &mut Env) -> Result<Option<Val>, String> {
-    let parse = nonnu::parse(input).map_err(|msg| format!("parse error: {}", msg))?;
-    let evaluated = parse.eval(env).map_err(|msg| format!("evaluation error: {}", msg))?;
-
-    if evaluated == nonnu::Val::Unit {
-        Ok(None)
-    } else {
-        Ok(Some(evaluated))
-    }
+fn main() {
+    println!("Hello, world!");
 }
--- crates/nonnu/src/lib.rs
+++ crates/nonnu/src/lib.rs
@@ -1,28 +1,7 @@
-pub mod binding_def;
-pub mod env;
-pub mod expr;
-pub mod func_def;
-pub mod stmt;
-pub mod val;
-pub use env::Env;
-pub use val::Val;
-
-mod utils;
-
-#[derive(Debug)]
-pub struct Parse(stmt::Stmt);
-
-impl Parse {
-    pub fn eval(&self, env: &mut Env) -> Result<Val, String> {
-        self.0.eval(env)
-    }
-}
-
-pub fn parse(s: &str) -> Result<Parse, String> {
-    let (s, stmt) = stmt::Stmt::new(s)?;
-    if s.is_empty() {
-        Ok(Parse(stmt))
-    } else {
-        Err("input was not consumed fully by parser".to_string())
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
     }
 }
