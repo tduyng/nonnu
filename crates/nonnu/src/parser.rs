d55e05bda77a75033412fb676de69c1e98240182
tduyng <tienduy.nguyen.dev@gmail.com>
2024-04-25 16:01:29 +0200

    feat: add parser

--- crates/nonnu/src/parser.rs
+++ crates/nonnu/src/parser.rs
@@ -1,1 +1,68 @@
+use logos::Logos;
+use rowan::{GreenNode, GreenNodeBuilder, Language};
 
+use crate::{
+    lexer::SyntaxKind,
+    syntax::{NonnuLanguage, SyntaxNode},
+};
+
+pub struct Parser<'a> {
+    pub lexer: logos::Lexer<'a, SyntaxKind>,
+    pub builder: GreenNodeBuilder<'static>,
+}
+
+impl<'a> Parser<'a> {
+    pub fn new(input: &'a str) -> Self {
+        Self {
+            lexer: SyntaxKind::lexer(input),
+            builder: GreenNodeBuilder::new(),
+        }
+    }
+
+    pub fn parse(mut self) -> Parse {
+        self.start_node(SyntaxKind::Root);
+        self.finish_node();
+
+        Parse {
+            green_node: self.builder.finish(),
+        }
+    }
+
+    fn start_node(&mut self, kind: SyntaxKind) {
+        self.builder.start_node(NonnuLanguage::kind_to_raw(kind));
+    }
+
+    fn finish_node(&mut self) {
+        self.builder.finish_node()
+    }
+}
+
+pub struct Parse {
+    green_node: GreenNode,
+}
+
+impl Parse {
+    pub fn debug_tree(&self) -> String {
+        let syntax_node = SyntaxNode::new_root(self.green_node.clone());
+        let formatted = format!("{:#?}", syntax_node);
+
+        // remove new line at the end
+        formatted[0..formatted.len() - 1].to_string()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use expect_test::{expect, Expect};
+
+    fn check(input: &str, expected_tree: Expect) {
+        let parse = Parser::new(input).parse();
+        expected_tree.assert_eq(&parse.debug_tree());
+    }
+
+    #[test]
+    fn parse_nothing() {
+        check("", expect![[r#"Root@0..0"#]]);
+    }
+}
